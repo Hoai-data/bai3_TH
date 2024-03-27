2fb948fa753c25a45acbaf21c31524754ca28f02
Hoai <thuhoai12042003@gmail.com>
2024-03-27 14:28:15 +0700

    Commit 2

--- bai3_TH/Data/ApplicationDbContext.cs
+++ bai3_TH/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿
 using bai3_TH.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 namespace bai3_TH.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
@@ -12,6 +13,7 @@ namespace bai3_TH.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<ApplicationUser> applicationUsers { get; set; }
     }
 
 }
--- bai3_TH/Program.cs
+++ bai3_TH/Program.cs
@@ -1,5 +1,7 @@
 using bai3_TH.Data;
+using bai3_TH.Models;
 using bai3_TH.Repository;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+//builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+//
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+ .AddDefaultTokenProviders()
+ .AddDefaultUI()
+ .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -30,6 +41,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+//
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
